66618bf9305372d778b422e8c1d0ab180de1fc1b
KeisakuHiga <khiga2943@gmail.com>
2021-05-20 19:50:09 +0900

    succeeded connecting postgresql

--- src/providers/Database.ts
+++ src/providers/Database.ts
@@ -2,26 +2,23 @@ import { Client } from 'pg';
 import Locals from './Locals';
 import Log from '../middlewares/Log';
 
+const client = new Client({
+  user: Locals.config().PgUser,
+  host: Locals.config().PgHost,
+  database: Locals.config().PgDatabase,
+  password: Locals.config().PgPassword,
+  port: Locals.config().PgPort,
+});
+
 export class Database {
   public static init(): any {
-		const client = new Client({
-			user: Locals.config().PgPort,
-			host: Locals.config().PgHost,
-			database: Locals.config().PgDatabase,
-			password: Locals.config().PgPassword,
-			port: Locals.config().PgPort
-		});
-		client.connect((err) => {
+    client.connect((err) => {
       if (err) {
 				Log.error(err.stack);
 				Log.error(`database connection error:\n${err.stack}`);
-			} else {
-				Log.info('database connected');
 			}
     });
-    // client.query('SELECT $1::text as message', ['Hello world!'], (err: { stack: any; }, res: { rows: { message: any; }[]; }) => {
-		// 	console.log(err ? err.stack : res.rows[0].message); // Hello World!
-		// 	client.end();
-		// });
   }
 }
+
+export default client;
