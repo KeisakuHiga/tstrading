a9d6ab81527dcf91dd79d26cfb94fd80cc1a7a55
KeisakuHiga <khiga2943@gmail.com>
2021-06-09 22:59:10 +0900

    comment out

--- docs/ddls/021_insert_btc_jpy_second.sql
+++ docs/ddls/021_insert_btc_jpy_second.sql
@@ -1,17 +1,17 @@
-INSERT INTO btc_jpy_second
-  (
-  time,
-  open,
-  close,
-  high,
-  low,
-  volume
-  )
-VALUES(
-    '2021-06-01T03:30:14.000Z',
-    4322910,
-    4842910,
-    5082910,
-    4222910,
-    100000000
-);
+-- INSERT INTO btc_jpy_second
+--   (
+--   time,
+--   open,
+--   close,
+--   high,
+--   low,
+--   volume
+--   )
+-- VALUES(
+--     '2021-06-01T03:30:14.000Z',
+--     4322910,
+--     4842910,
+--     5082910,
+--     4222910,
+--     100000000
+-- );
